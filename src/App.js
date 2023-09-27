3034fe764f04dd6796bf4f60d5bb139025b63f72
sree <sreelakshmiskumar45@gmail.com>
2023-09-27 02:11:36 -0700

    netflix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Banner from './components/Banner';
+import Row from './components/Row';
+import requests from './requests';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Netflix</h1>
+    <div >
+      <Navbar/>
+     <Banner fetchUrl={requests.fetchNetflixOriginals} />
+      <Row isPoster={true} title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending In India" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated Movies" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horrer Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
+
+
     </div>
   );
 }
